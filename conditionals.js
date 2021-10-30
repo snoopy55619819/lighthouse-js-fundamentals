fa051ce65520194e685f45f4512cca508241847c
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2021-10-30 03:41:04 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -7,3 +7,21 @@ if (temperature < 0) {
 } else {
   console.log("Short sleeves are fine");
 }
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature >40) {
+  console.log("Maybe going outside is not such a great idea...");
+}
+
+const raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
