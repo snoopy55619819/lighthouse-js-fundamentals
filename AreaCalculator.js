97cbdd16a320554cb0303ec9d6df4f24dd0b2248
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2021-11-02 04:06:41 +0000

    Calculate are of rectangle, triange and circle.

---
+++ AreaCalculator.js
@@ -0,0 +1,39 @@
+//Calculate area of rectangle.
+const calculateRectangleArea = function(length, width) {
+  if (length < 0 || width < 0) {
+    return undefined;
+  }
+  return (length * width);
+}
+
+//Calculate area of triangle.
+const calculateTriangleArea = function(base, height) {
+  if (base < 0 || height < 0) {
+    return undefined;
+  }
+  return (base * height / 2);
+}
+
+//Calculate area of circle.
+const calculateCircleArea = function(radius) {
+  if (radius < 0) {
+    return undefined;
+  }
+  const piValue = Math.PI;
+  return (piValue * Math.pow(radius,2));
+}
+
+
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
+
