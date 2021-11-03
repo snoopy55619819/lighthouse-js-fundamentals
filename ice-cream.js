13236ad1e23103d07d0e9d60abb1755170151799
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2021-11-03 01:17:30 +0000

    Ice-Cream assignment outputs

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");//add root beer to flavors.
+console.log(iceCreamFlavours);//print updated array of flavors.
+
+console.log(iceCreamFlavours[0]);//print first item in array.
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);//prints last item in array.
+
+console.log(iceCreamFlavours.length);//prints length of array.
