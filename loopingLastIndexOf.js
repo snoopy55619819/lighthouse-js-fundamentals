debd4c1e3da0423425c11b2fb91bdac1958b9385
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2021-11-03 02:42:14 +0000

    Return last index of a value in an array.

---
+++ loopingLastIndexOf.js
@@ -0,0 +1,11 @@
+//Loop through array and return last index of value.
+function lastIndexOf(array, value) {
+  return (array.lastIndexOf(value));
+}
+
+//Tests:
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
