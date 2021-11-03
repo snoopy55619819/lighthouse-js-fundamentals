7981a8d193022f731dac677bb71153b4011ef982
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2021-11-03 03:23:59 +0000

    Concatenate 2 arrays and return resulting array.

---
+++ concatenateArrays.js
@@ -0,0 +1,10 @@
+//Concatenate 2 arrays and return resulting array
+function concat(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+//Tests:
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
